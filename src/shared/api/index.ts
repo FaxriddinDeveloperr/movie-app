2b2448d00c021ec045fe3b52ea7130513fcd5788
FaxriddinDeveloperr <fazliddinomariipov@gmail.com>
2025-09-20 16:02:05 +0500

    project done

--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -1,14 +1,17 @@
 import axios from "axios";
-import { BASE_URL, TOKEN } from "../const";
+import { BASE_URL, AUTH_BASE_URL, TOKEN } from "../const";
 
+// TMDB API
 export const api = axios.create({
   baseURL: BASE_URL,
 });
 
+// Backend API
 export const authApi = axios.create({
-  baseURL: BASE_URL,
+  baseURL: AUTH_BASE_URL,
 });
 
+// Token faqat TMDB uchun kerak
 api.interceptors.request.use((config) => {
   if (TOKEN) {
     config.headers.Authorization = `Bearer ${TOKEN}`;
--- src/shared/const/index.ts
+++ src/shared/const/index.ts
@@ -1,6 +1,6 @@
 const meta = import.meta.env;
 
 export const IMAGE_URL = meta.VITE_IMAGE_URL;
-export const BASE_URL = meta.VITE_BASE_URL;
-export const AUTH_BASE_URL = meta.VITE_AUTH_URL;
+export const BASE_URL = meta.VITE_BASE_URL;        // TMDB uchun
+export const AUTH_BASE_URL = meta.VITE_AUTH_URL;   // backend uchun
 export const TOKEN = meta.VITE_TOKEN;
