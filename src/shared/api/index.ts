405a66a017f14b983122d2d3d47b280b9279024b
FaxriddinDeveloperr <fazliddinomariipov@gmail.com>
2025-09-20 15:50:50 +0500

    project done

--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { BASE_URL } from "../const";
+import { BASE_URL, TOKEN } from "../const";
 
 export const api = axios.create({
   baseURL: BASE_URL,
@@ -10,10 +10,9 @@ export const authApi = axios.create({
 });
 
 api.interceptors.request.use((config) => {
-  const token =
-    "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlM2U0Y2M0YzA2YzcwN2I0ODcyMWVlY2ZjMjE5MGVmYyIsIm5iZiI6MTcyODg3NzQyNi4yLCJzdWIiOiI2NzBjOTM3MmIxNWQ5N2IxYTkzY2UwZjIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.zZR_akio6SBvGWR4ThRbmrrWDuHZukkom4xo091rw8U";
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+  if (TOKEN) {
+    config.headers.Authorization = `Bearer ${TOKEN}`;
   }
+  config.headers["Cache-Control"] = "no-cache";
   return config;
 });
--- src/shared/const/index.ts
+++ src/shared/const/index.ts
@@ -2,4 +2,5 @@ const meta = import.meta.env;
 
 export const IMAGE_URL = meta.VITE_IMAGE_URL;
 export const BASE_URL = meta.VITE_BASE_URL;
-export const AUTH_BASE_URL = meta.VITE_AUTH_BASE_URL;
+export const AUTH_BASE_URL = meta.VITE_AUTH_URL;
+export const TOKEN = meta.VITE_TOKEN;
